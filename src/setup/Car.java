38ecaa3311a2c2f3cefbab12fd7652ca45ee7ccd
erikachlupsa <erika.chlupsa@gmail.com>
2021-10-03 14:37:57 -0500

    made car IN MASTER BRANCH

---
+++ src/setup/Car.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Car {
+
+    // this is car class FROM MASTER BRANCH
+
+    // we will have conflict
+
+    // because there is different text in different branches for car
+}
